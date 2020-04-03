c33f7b74da496117492b665213da0e573769d96d
markozg01 <marko.zg01@gmail.com>
2020-04-03 13:42:34 +0200

    - my task

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,21 +1,54 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by jt on 12/26/19.
  */
 @Controller
-public class MyController {
+public class MyController implements InitializingBean {
 
-    private final GreetingService greetingService;
+    @Value("string value")
+    private String stringValue;
 
-    public MyController(GreetingService greetingService) {
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public void setStringValue(String stringValue) {
+        System.out.println("SET STRING VALUE");
+        this.stringValue = stringValue;
+    }
+
+    private GreetingService greetingService;
+
+    public GreetingService getGreetingService() {
+        return greetingService;
+    }
+
+    @Autowired
+    public void setGreetingService(GreetingService greetingService) {
+        System.out.println("CALLED SETTING GREETING SERVICE");
         this.greetingService = greetingService;
     }
 
     public String sayHello(){
         return greetingService.sayGreeting();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NAKON ŠTO SU PROPERTYI POSTAVLJENI..");
+    }
+
+    @PostConstruct
+    public void test(){
+        System.out.println("NAKON ŠTO JE BEAN GOTOV ALI PRIJE KORIŠTENJA");
     }
 }
